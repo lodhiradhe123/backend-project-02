571fbda733b04fb4856ce5cc5aca665e7f7b4eb5
Radehshyam Lodhi <radheshyamlodhi48@gmail.com>
2024-06-21 02:52:18 +0530

    done

--- models/user.js
+++ models/user.js
@@ -2,13 +2,29 @@ const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
  
 const uerSchema = mongoose.Schema({
-    name:String,
-    username:String,
-    email:{
-        type:String,
-        unique:true,
+    name:{
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        minlength: [3, 'Name must be at least 3 characters long']
     },
-    password:String,
+    username:{
+        type: String,
+        required: [true, 'username is required'],
+        trim: true,
+        minlength: [3, 'username must be at least 3 characters long']
+    },
+    email:{
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password:{
+        type: String,
+        
+    },
     role:{type:String,
         enum:["seller","user"]
     }
